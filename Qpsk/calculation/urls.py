2afc8c7bd0e07670c343440512222c1e6efb365c
Егор Болычев <egorbolycev@MacBook-Pro-Egor.local>
2023-09-12 10:37:08 +0300

    v2

--- Qpsk/calculation/urls.py
+++ Qpsk/calculation/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     re_path(r'^protocol/$', ProtocolList.as_view()),
     re_path(r'^protocol_params/$', ProtocolParams.as_view()),
     re_path(r'^index/$', Index.as_view()),
-    re_path(r'^index/<int:task_id>$', Index.as_view()),
+    re_path(r'^index/(?P<task_id>[a-zA-Z0-9-_]+)$', Index.as_view()),
 ]
